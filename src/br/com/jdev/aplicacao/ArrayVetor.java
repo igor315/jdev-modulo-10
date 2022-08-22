01a9f997ce11a95dc1917b6f4e364aff163a280a
Igor Santos Oliveira <iigor315@hotmail.com>
2022-08-22 18:55:43 -0300

    INICIANDO MÓDULO 11 Criando nosso primeiro Array

---
+++ src/br/com/jdev/aplicacao/ArrayVetor.java
@@ -0,0 +1,18 @@
+package br.com.jdev.aplicacao;
+
+public class ArrayVetor {
+
+	public static void main(String[] args) {
+		
+//		Array pode ser de todos os tipos de dados e objetos
+//		Array sempre deve ter uma quantidade de posições definidas
+		double[] notas = new double[4];
+		notas[0] = 7.84;
+		notas[1] = 5.1;
+		notas[2] = 9.2;
+		notas[3] = 6.3;
+
+		System.out.println(notas[2]);
+	}
+
+}
