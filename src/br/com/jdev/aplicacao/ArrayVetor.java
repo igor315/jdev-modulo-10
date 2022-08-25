98a9e404b47d5b7463678b4f5692216cf4ed7e56
Igor Santos Oliveira <iigor315@hotmail.com>
2022-08-25 11:58:46 -0300

    Diferentes formas de atribuição de valores

--- src/br/com/jdev/aplicacao/ArrayVetor.java
+++ src/br/com/jdev/aplicacao/ArrayVetor.java
@@ -6,17 +6,21 @@ public class ArrayVetor {
 
 	public static void main(String[] args) {
 
-		String posicoes = JOptionPane.showInputDialog("Quantas posicoes o Array deve ter?");
-//		Array sempre deve ter a quantidade de posicoes definidas
-		double[] notas = new double[Integer.parseInt(posicoes)];
+//                          0 	 1    2	
+		double[] numeros = {9.5, 8.4, 5.5};
 		
-		for (int pos = 0; pos < notas.length; pos++) {
-			String valor = JOptionPane.showInputDialog("Qual o valor da posição = " + (pos+1));
-			notas[pos] = Double.valueOf(valor);
-		}
-
-		for (int pos = 0; pos < notas.length; pos++) {
-			System.out.println("Nota " + (pos + 1) + " = " + notas[pos]);
+//		Quando são varios tipos de dados fazemos um Array de String
+		String[] dados = new String[4];
+		dados[0] = "Igor";
+		dados[1] = "Curso de Java";
+		dados[2] = "igor312@gmail.com.br";
+		dados[3] = "idade 27 anos";
+		
+//		Mesmo Array de cima feito de outra forma
+		String[] valores = {"Igor", "Curso de Java", "igor312@gmail.com.br", "idade 27 anos"};
+		
+		for(int pos = 0; pos < valores.length; pos ++) {
+			System.out.println("Valor na posição " + pos + " é = " +valores[pos]);
 		}
 	}
 
