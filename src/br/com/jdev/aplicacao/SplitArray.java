6a53a8bf5656d650fcb0cda857822f2790384e9f
Igor Santos Oliveira <iigor315@hotmail.com>
2022-08-31 17:44:38 -0300

    Conversão Array para Lista e Lista para Array

--- src/br/com/jdev/aplicacao/SplitArray.java
+++ src/br/com/jdev/aplicacao/SplitArray.java
@@ -1,14 +1,16 @@
 package br.com.jdev.aplicacao;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SplitArray {
 
 	public static void main(String[] args) {
 		
-		String texto = "Igor, curso Java, 80, 75, 60, 90";
+		String texto = " Igor, curso Java, 80, 75, 60, 90";
 		
 		String[] valoresArray = texto.split(",");
 		
-		System.out.println(valoresArray);
 		
 		System.out.println("Nome: " + valoresArray[0]);
 		System.out.println("Curso: " + valoresArray[1]);
@@ -16,6 +18,26 @@ public class SplitArray {
 		System.out.println("Nota 2: " + valoresArray[3]);
 		System.out.println("Nota 3: " + valoresArray[4]);
 		System.out.println("Nota 4: " + valoresArray[5]);
+		
+		System.out.println("===================================================");
+		
+		
+//		Convertendo um Array em uma lista
+		List<String> lista = Arrays.asList(valoresArray);
+		
+		for (String valorString : lista) {
+			System.out.println(valorString);
+		}
+		
+		System.out.println("===================================================");
+		
+//		Convertendo uma lista para um array
+		String[] conversaoArray = lista.toArray(new String[6]);
+		
+		
+		for(int pos = 0; pos < conversaoArray.length; pos++) {
+			System.out.println(conversaoArray[pos]);
+		}
 	}
 
 }
